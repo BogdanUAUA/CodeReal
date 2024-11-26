7c3be3dff09dc8b598f3d670bb0ca3cf2efc572c
BogdanUAUA <144799495+BogdanUAUA@users.noreply.github.com>
2024-11-26 21:31:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("miBoton").addEventListener("click", () => {
+    alert("¡Gracias por visitar CodeReal!");
+    document.querySelector("h1").innerText = "¡Bienvenido a CodeReal!";
+    document.body.style.backgroundColor = "#e8f5e9"; // Cambia el fondo a un verde claro
+});
